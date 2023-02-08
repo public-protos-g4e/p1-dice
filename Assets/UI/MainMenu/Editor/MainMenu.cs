613c6ccaec9ff517e317a4825e9b3893f63647f1
Franco Ferrari <francoandres.ferrari@globant.com>
2023-02-08 14:49:59 -0300

    Created Spash Scene. Created Main menu

---
+++ Assets/UI/MainMenu/Editor/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor;
+using UnityEditor.UIElements;
+
+public class MainMenu : EditorWindow
+{
+    private VisualElement container;
+    public const string path = "Assets/UI/MainMenu/Editor/";
+
+
+    [MenuItem("ChiriDice/Main menu")]
+    public static void ShowWindow()
+    {
+        MainMenu window = GetWindow<MainMenu>();
+        window.titleContent = new GUIContent("Main menu");
+        window.minSize = new Vector2(300, 400);
+    }
+
+    public void CreateGUI()
+    {
+        this.container = rootVisualElement;
+
+        // Load uxml file
+        VisualTreeAsset visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(path + "MainMenu.uxml");
+        this.container.Add(visualTree.Instantiate());
+
+        // Load uss file
+        StyleSheet styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(path + "MainMenu.uss");
+        this.container.styleSheets.Add(styleSheet);
+    }
+}
---
+++ Assets/UI/Splash/Editor/SplashScreen.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor;
+using UnityEditor.UIElements;
+
+public class Splash : EditorWindow
+{
+    VisualElement container;
+
+    [MenuItem("ChiriDice/Splash")]
+    public static void ShowWindow()
+    {
+        Splash window = GetWindow<Splash>();
+        window.titleContent = new GUIContent("UISplash");
+    }
+    
+    public void CreateGUI()
+    {
+        this.container = rootVisualElement;
+        VisualTreeAsset visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UI/SplashScreen.uxml");
+        this.container.Add(visualTree.Instantiate());
+
+        StyleSheet styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/UI/SplashScreen.uss");
+        this.container.styleSheets.Add(styleSheet);
+    }
+}
