3828f3f666d8cf253bec232b3e29492553483873
wmunoz <wmunoz117@gmail.com>
2023-03-02 12:19:29 -0300

    Fix detect if dice is not moving with IsSleeping()

--- Assets/Scripts/Shake.cs
+++ Assets/Scripts/Shake.cs
@@ -11,27 +11,30 @@ public class Shake : MonoBehaviour
 
     private bool checkvalue = false;
 
-    public void Start() {
-        this.rb   = gameObject.GetComponent<Rigidbody>();
-        this.tr   = gameObject.GetComponent<Transform>();
+    public void Start()
+    {
+        this.rb = gameObject.GetComponent<Rigidbody>();
+        this.tr = gameObject.GetComponent<Transform>();
         this.dice = gameObject.GetComponent<Die_d10>();
     }
 
-    public void Update() {
-        
+    public void Update()
+    {
         // if(this.shake == true) {
         //     Debug.Log("jump");
         //     this.shake = false;
         //     this.DoIt();
         // }
 
-        if(!dice.rolling && checkvalue) {
+        if (rb.IsSleeping() && checkvalue)
+        {
             checkvalue = false;
             Debug.Log("Add score: " + gameObject.GetComponent<Die_d10>().value);
             GameManager.Instance.AddScore(gameObject.GetComponent<Die_d10>().value);
         }
 
-        if(Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             DoIt();
             checkvalue = true;
         }
@@ -49,7 +52,6 @@ public class Shake : MonoBehaviour
 
     public void OnTriggerStay(Collider other)
     {
-        
     }
 
     private float GetRandom(float min, float max)
